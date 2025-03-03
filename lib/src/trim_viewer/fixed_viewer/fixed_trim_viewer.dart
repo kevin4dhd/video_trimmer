e84ce5520c00a282abaac583f4b82f8278c75f84
“rafahig” <mc@hi-g.io>
2025-03-03 10:10:21 -0400

    fix

--- lib/src/trim_viewer/fixed_viewer/fixed_trim_viewer.dart
+++ lib/src/trim_viewer/fixed_viewer/fixed_trim_viewer.dart
@@ -505,7 +505,7 @@ class _FixedTrimViewerState extends State<FixedTrimViewer>
                     padding: const EdgeInsets.all(8.0),
                     child: Text(
                       Duration(
-                        milliseconds: math.min(
+                        milliseconds: math.max(
                           _videoEndPos.toInt() - _videoStartPos.toInt(),
                           1000,
                         ),
