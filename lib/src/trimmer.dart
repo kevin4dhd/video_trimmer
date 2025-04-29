b735033b5a778542310ff84ba83de0fe44bae542
Kevin Piazzoli Felix Perez <kf@hi-g.io>
2025-04-29 15:42:19 -0500

    ffmpeg kit min gpl

--- lib/src/trimmer.dart
+++ lib/src/trimmer.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
+import 'package:ffmpeg_kit/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit/return_code.dart';
 import 'package:path/path.dart';
 
 import 'package:flutter/material.dart';
@@ -73,8 +73,9 @@ class Trimmer {
     }
 
     // Directory + folder name
-    final Directory directoryFolder =
-        Directory('${directory!.path}/$folderName/');
+    final Directory directoryFolder = Directory(
+      '${directory!.path}/$folderName/',
+    );
 
     if (await directoryFolder.exists()) {
       // If folder already exists return path
@@ -83,8 +84,9 @@ class Trimmer {
     } else {
       debugPrint('Creating');
       // If folder does not exists create folder and then return its path
-      final Directory directoryNewFolder =
-          await directoryFolder.create(recursive: true);
+      final Directory directoryNewFolder = await directoryFolder.create(
+        recursive: true,
+      );
       return directoryNewFolder.path;
     }
   }
@@ -198,9 +200,7 @@ class Trimmer {
     String path = await _createFolderInAppDocDir(
       videoFolderName,
       storageDir,
-    ).whenComplete(
-      () => debugPrint("Retrieved Trimmer folder"),
-    );
+    ).whenComplete(() => debugPrint("Retrieved Trimmer folder"));
 
     Duration startPoint = Duration(milliseconds: startValue.toInt());
     Duration endPoint = Duration(milliseconds: endValue.toInt());
@@ -244,8 +244,9 @@ class Trimmer {
     command += '"$outputPath"';
 
     FFmpegKit.executeAsync(command, (session) async {
-      final state =
-          FFmpegKitConfig.sessionStateToString(await session.getState());
+      final state = FFmpegKitConfig.sessionStateToString(
+        await session.getState(),
+      );
       final returnCode = await session.getReturnCode();
 
       debugPrint("FFmpeg process exited with state $state and rc $returnCode");
@@ -282,8 +283,9 @@ class Trimmer {
     } else {
       if (videoPlayerController!.value.position.inMilliseconds >=
           endValue.toInt()) {
-        await videoPlayerController!
-            .seekTo(Duration(milliseconds: startValue.toInt()));
+        await videoPlayerController!.seekTo(
+          Duration(milliseconds: startValue.toInt()),
+        );
         await videoPlayerController!.play();
         return true;
       } else {
